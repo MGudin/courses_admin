bc709b68c209162b90f9bf285af69d64f8ebda43
chudix <stkboy@gmail.com>
2017-12-13 20:44:18 -0300

    add constraint to courses migration

--- db/migrate/20171213192650_create_courses.rb
+++ db/migrate/20171213192650_create_courses.rb
@@ -1,7 +1,7 @@
 class CreateCourses < ActiveRecord::Migration[5.1]
   def change
     create_table :courses do |t|
-      t.integer :year, null: false
+      t.integer :year, null: false, unique: true
 
       t.timestamps
     end
