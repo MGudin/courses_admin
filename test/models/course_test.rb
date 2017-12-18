4995f49015f534f5dd66354eaf36dcffd606553e
chudix <stkboy@gmail.com>
2017-12-18 17:24:37 -0300

    modifies courses test

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -3,12 +3,12 @@ require 'test_helper'
 class CourseTest < ActiveSupport::TestCase
   test "course shouldnt save without a year" do
       course = Course.new()
-      assert_not course.save
+      assert_not course.valid?
   end
 
   test "course year should be unique" do
     course = Course.new()
     course.year = 2017
-    assert_not course.save
+    assert_not course.valid?
   end
 end
