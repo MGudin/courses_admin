f261a782399cc10a432a1f85247199ab76a064d7
chudix <stkboy@gmail.com>
2017-12-18 17:20:21 -0300

    adds validations to students model and tests

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,28 @@ class Student < ApplicationRecord
 
   has_many :grades
   has_many :exams, through: :grades
+
+  # Validations
+  #   t.string "name", limit: 20, null: false
+  validates :name, presence: true
+  validates :name, length: {maximum: 20}
+  validates :name, length: {minimum: 1}
+  # t.string "last_name", limit: 20, null: false
+  validates :last_name, presence: true
+  validates :last_name, length: {maximum: 20}
+  validates :last_name, length: {minimum: 1}
+  # t.integer "dni", null: false
+  validates :dni, presence: true
+  validates :dni, numericality: { only_integer: true}
+  validates :dni, numericality: { greater_than: 0}
+
+  # t.integer "student_number", null: false
+  validates :student_number, presence: true
+  validates :dni, numericality: { only_integer: true}
+  validates :dni, numericality: { greater_than: 0}
+
+  # t.bigint "course_id", null: false
+  validates :course_id, presence: true
+
+  
 end
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,82 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "test student cant be saved without name" do
+    student = student_with_attr name: nil
+    assert_not student.valid?
+  end
+
+  test "test student cant be saved without last name" do
+    student = student_with_attr last_name: nil
+    assert_not student.valid?
+  end
+
+  test "test student cant be saved without dni" do
+    student = student_with_attr dni: nil
+    assert_not student.valid?
+  end
+
+  test "test student cant be saved without student_number" do
+    student = student_with_attr student_number: nil
+    assert_not student.valid?
+  end
+  
+  test "test student cant be saved without course_id" do
+    student = student_with_attr course_id: nil
+    assert_not student.valid?
+  end
+
+  test "name size should not be greater than 20" do
+    student = student_with_attr name: "un nombre realmente muy largo"
+    assert_not student.valid?
+  end
+
+  test "name size should not be greater than 0" do
+    student = student_with_attr name: ""
+    assert_not student.valid?
+  end
+
+  test "last_name size should not be greater than 20" do
+    student = student_with_attr last_name: "un apellido realmente muy largo"
+    assert_not student.valid?
+  end
+
+  test "last_name size should not be greater than 0" do
+    student = student_with_attr last_name: ""
+    assert_not student.valid?
+  end
+
+  test "dni should be an integer value" do
+    student = student_with_attr dni: 12.34
+    assert_not student.valid?
+  end
+
+  test "dni should be greater than zero" do
+    student = student_with_attr dni: -1
+    assert_not student.valid?
+  end
+  test "student_number should be an integer value" do
+    student = student_with_attr student_number: 12.34
+    assert_not student.valid?
+  end
+
+  test "student_number should be greater than zero" do
+    student = student_with_attr student_number: -1
+    assert_not student.valid?
+  end
+  
+  private
+  def student_with_attr **attrs
+    s = sample_student
+    s.attributes=attrs
+    s
+  end
+
+  def sample_student
+    Student.new(name: "John",
+                last_name: "Doe",
+                dni: 32333444,
+                student_number: 123232,
+                course_id: 1)
+  end
 end
