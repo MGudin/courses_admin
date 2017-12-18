483264b90d20c7a7a46be3840e0072320360a8c2
chudix <stkboy@gmail.com>
2017-12-18 17:18:45 -0300

    modifies exam tests

--- test/models/exam_test.rb
+++ test/models/exam_test.rb
@@ -10,32 +10,32 @@ class ExamTest < ActiveSupport::TestCase
 
     test "exam shouldnt save when title is missing" do
       exam = exam_with_attr title: nil
-      assert_not exam.save
+      assert_not exam.valid?
     end
 
     test "exam shouldnt save when 'min grade' is missing" do
       exam = exam_with_attr min_grade: nil
-      assert_not exam.save
+      assert_not exam.valid?
     end
 
     test "exam shouldnt save when 'exam date' is missing" do
       exam = exam_with_attr exam_date: nil
-      assert_not exam.save
+      assert_not exam.valid?
     end
 
     test "exam shouldnt save when there isnt course related" do
       exam = exam_with_attr course_id: nil
-      assert_not exam.save
+      assert_not exam.valid?
     end
 
     test "min grade should be greater than zero" do
       exam = exam_with_attr min_grade: -1
-      assert_not exam.save
+      assert_not exam.valid?
     end  
 
     test "title length should be less than 20" do
       exam = exam_with_attr title: "este titulo tiene mas de 20 caracteres"
-      assert_not exam.save
+      assert_not exam.valid?
     end
     private
     def exam_with_attr **attrs
