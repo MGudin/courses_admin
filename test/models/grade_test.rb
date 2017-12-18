9a89842788760b70b3f5b951517b49ea0285b2d0
chudix <stkboy@gmail.com>
2017-12-18 18:26:14 -0300

    WIP: grades testing

--- test/models/grade_test.rb
+++ test/models/grade_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class GradeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "test grade should be present" do
+    grade = grade_with_attr grade: nil
+    assert_not grade.valid?
+  end
+  private
+  def sample_grade
+    Grade.new(grade: 6,
+              course_id: 1,
+              exam_id: 2,
+              student_id: 2)
+  end
+
+  def grade_with_attr **attrs
+    g = sample_grade
+    g.attributes= attrs
+    g
+  end
+              
 end
