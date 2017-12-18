d4e5e105cc42550a70e7228774f6ff4be5563210
chudix <stkboy@gmail.com>
2017-12-18 13:24:19 -0300

    adds some tests to course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,8 @@ class Course < ApplicationRecord
   has_many :exams
   has_many :students
 
+  # Validations
+  validates :year, presence: true, uniqueness: true
+
   scope :exams_by_year, -> (a_year) { find_by(year: a_year).exams}
 end
--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "course shouldnt save without a year" do
+      course = Course.new()
+      assert_not course.save
+  end
+
+  test "course year should be unique" do
+    course = Course.new()
+    course.year = 2017
+    assert_not course.save
+  end
 end
