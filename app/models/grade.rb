78630ff9fb13dffdd736e8d34438bf435a66ee6b
chudix <stkboy@gmail.com>
2017-12-19 12:42:46 -0300

    adds tests to grade model

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -2,4 +2,12 @@ class Grade < ApplicationRecord
   belongs_to :student
   belongs_to :exam
   belongs_to :course
+
+  # Validations
+  ## grade
+  validates :grade, presence: true
+  validates :grade, numericality: true
+  validates :grade, numericality: { greater_than_or_equal_to: 0}
+  ## exam_id, student_id index
+  validates :exam, uniqueness: { scope: :student }
 end
--- test/models/grade_test.rb
+++ test/models/grade_test.rb
@@ -6,12 +6,36 @@ class GradeTest < ActiveSupport::TestCase
     grade = grade_with_attr grade: nil
     assert_not grade.valid?
   end
+
+  test "test grade should be numeric" do
+    grade = grade_with_attr grade: "un string"
+    assert_not grade.valid?
+  end
+
+  test "test grade should be positive" do
+    grade = grade_with_attr grade: -1
+    assert_not grade.valid?
+  end
+
+  test "test grade couldnt be saved without exam, course and student" do
+    grade = Grade.new(grade: 7)
+    assert_not grade.save
+  end
+
+  test "test grade cant be saved if exam_id,student_id already exists" do
+    grade = sample_grade()
+    grade.save
+    another_grade = sample_grade()
+    another_grade.course = Course.last
+    assert_not another_grade.save
+  end
+  
   private
   def sample_grade
     Grade.new(grade: 6,
-              course_id: Course.first,
-              exam_id: Exam.first,
-              student_id: Student.first)
+              course: Course.first,
+              exam: Exam.first,
+              student: Student.first)
   end
 
   def grade_with_attr **attrs
