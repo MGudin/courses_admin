3914f2ddf68c2a3b343142ebcae8dd1c023e1b74
chudix <stkboy@gmail.com>
2017-12-20 19:16:31 -0300

    fixes student attr issue

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -69,6 +69,6 @@ class StudentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def student_params
-      params.require(:student).permit(:name, :last_name, :dni, :studen_number)
+      params.require(:student).permit(:name, :last_name, :dni, :student_number)
     end
 end
