8a9f9d7dfb7ef5e9f53b36da1ac6024b92ea883f
chudix <stkboy@gmail.com>
2017-12-21 09:18:38 -0300

    WIP: courses actions

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -15,10 +15,12 @@ class CoursesController < ApplicationController
   # GET /courses/new
   def new
     @course = Course.new
+    @action = "Crear"
   end
 
   # GET /courses/1/edit
   def edit
+    @action = "Modificar"
   end
 
   # POST /courses
@@ -28,7 +30,7 @@ class CoursesController < ApplicationController
 
     respond_to do |format|
       if @course.save
-        format.html { redirect_to @course, notice: 'Course was successfully created.' }
+        format.html { redirect_to @course, notice: 'La cursada se creo exitosamente.' }
         format.json { render :show, status: :created, location: @course }
       else
         format.html { render :new }
@@ -42,7 +44,7 @@ class CoursesController < ApplicationController
   def update
     respond_to do |format|
       if @course.update(course_params)
-        format.html { redirect_to @course, notice: 'Course was successfully updated.' }
+        format.html { redirect_to @course, notice: 'La cursada se actualizo exitosamente.' }
         format.json { render :show, status: :ok, location: @course }
       else
         format.html { render :edit }
@@ -56,7 +58,7 @@ class CoursesController < ApplicationController
   def destroy
     @course.destroy
     respond_to do |format|
-      format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
+      format.html { redirect_to courses_url, notice: 'La cursada se borro exitosamente.' }
       format.json { head :no_content }
     end
   end
