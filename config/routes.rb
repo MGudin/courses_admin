2d22aed455eece6c8b50141297d57b6b4664284b
chudix <stkboy@gmail.com>
2017-12-20 23:20:03 -0300

    WIP: simple_form integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :students
-  resources :exams
-  resources :courses
-  resources :grades
+  # resources :students
+  # resources :exams, only: [:index]
+  # resources :courses
+  # resources :grades, only: [:index]
+  # resources :students, only: [:index]
+  resources :courses do
+    resources :exams
+    resources :students
+  end
                                   
   root "courses#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
