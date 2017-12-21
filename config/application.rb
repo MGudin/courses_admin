c48285740bfd7cffab28985bded21194498f90b3
chudix <stkboy@gmail.com>
2017-12-21 07:48:36 -0300

    adds locales

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module CoursesAdmin
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.autoload_paths += %W["#{config.root}/app/validators/"]
+
+    config.i18n.available_locales = ['es-AR', :es, :en]
+    config.i18n.default_locale = :es
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
