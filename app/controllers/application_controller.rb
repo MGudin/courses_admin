34ffc1180bab0032ff32bb195d9bc9a05c9412c6
chudix <stkboy@gmail.com>
2017-12-21 17:24:49 -0300

    set @course available to all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_course
+
+  def set_course
+    @course = Course.find(params[:course_id] || params[:id])
+  end
 end
